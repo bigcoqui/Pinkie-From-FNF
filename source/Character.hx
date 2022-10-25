a5b29a4052ad972f007ffb213c582e46ee65fbcc
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-10-25 06:14:04 -0300

    update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -625,7 +625,7 @@ class Character extends FlxSprite
 			trace(curCharacter + ":" + path);
 
 			var gra:FlxGraphic;
-			var bmp = BitmapData.fromFile(path);
+			var bmp = BitmapData.fromAsset(path);
 			gra = FlxGraphic.fromBitmapData(bmp, false, char);
 			gra.persist = true;
 			charsBitmaps.set(char, gra);
